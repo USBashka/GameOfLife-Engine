92b7a95fa5f4e55bd64d1df99447d9ee2abeef99
USBashka <usbashka@gmail.com>
2022-10-18 18:24:20 +0400

    Improve UI, add FPS counter and ability to change speed

--- UI.gd
+++ UI.gd
@@ -2,6 +2,11 @@ extends Node
 
 @onready var compute = get_node("../LifeCompute") 
 
+func _process(delta):
+	get_node("FPS"). text = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS))
+
+
+
 func _on_randomise_button_pressed():
 	compute.randomise_data()
 
@@ -18,3 +23,8 @@ func _on_run_button_toggled(button_pressed):
 func _on_h_slider_value_changed(value):
 	get_node("../Camera2D").zoom = value * Vector2.ONE
 
+
+
+func _on_speed_value_changed(value):
+	compute.speed = value
+	get_node("VBoxContainer/SpeedLabel").text = "Speed: " + str(value)
